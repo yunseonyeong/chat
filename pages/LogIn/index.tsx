ee38a51f43324b544f7d6d428e7ad010a2017b04
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-07-31 15:44:04 +0900

    Redirect대신 useNavigate  or Navigate 사용하기

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import { Header, Error, Form, Label, Input, LinkContainer, Button } from './styles';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import useSWR from 'swr';
@@ -8,12 +8,14 @@ import fetcher from '@utils/fetcher';
 
 const LogIn = () => {
   //swr 장점 : 로딩 상태도 알 수 있다. 데이터가 존재하지 않으면(undefined) 로딩중임을 알 수 있음
-  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
-    dedupingInterval: 100000,
-  }); // 로그인 후 저장할 데이터, 해당 url을 fetcher로 옮겨준다.
+  // data, error가 바뀌면 리렌더링된다.
+  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher); // 로그인 후 저장할 데이터, 해당 url을 fetcher로 옮겨준다.
+
   const [email, , onChangeEmail] = useInput('');
   const [password, , onChangePassword] = useInput('');
   const [loginError, setLoginError] = useState(false);
+  //react router v6에서는 Redirect 지원하지 않음. useNavigate로 대체가능 하겠지?
+  // const navigate = useNavigate();
 
   const onSubmit = useCallback(
     (e) => {
@@ -33,16 +35,20 @@ const LogIn = () => {
         )
         .then((response) => {
           // swr최신버전에서 revalidate 가 사라졌다. mutate가 대체한다.
-          mutate();
+          mutate(response.data, false);
         })
         .catch((error) => {
           setLoginError(error.response?.status === 401);
         })
         .finally(() => {});
     },
-    [email, password, loginError],
+    [email, password, mutate],
   );
 
+  if (data) {
+    // 로그인 성공 시, 사용자 정보가 담긴 data값이 false에서 업데이트되면, Login컴포넌트가 리렌더링되고, channel로 라우팅
+    return <Navigate to="/workspace/channel" />;
+  }
   return (
     <div id="container">
       <Header>Sleact</Header>
